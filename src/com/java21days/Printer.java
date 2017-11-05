51d2a2452c4d091fd09941cbbc9b31f622a4b3ee
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-05 18:20:31 +0000

    new class

--- src/com/java21days/Printer.java
+++ src/com/java21days/Printer.java
@@ -1,4 +1,25 @@
 package com.java21days;
 
 public class Printer {
+    int x = 0;
+    int y = 1;
+
+    void printMe() {
+        System.out.println("x is " + x + ", y is " + y);
+        System.out.println("I am an instance of the class " + this.getClass().getName());
+    }
+}
+
+class SubPrinter extends Printer {
+    int z = 3;
+
+    void printMe() {
+        super.printMe();
+        System.out.println("Oh and also z is " + z);
+    }
+
+    public static void main(String[] arguments){
+        SubPrinter obj = new SubPrinter();
+        obj.printMe();
+    }
 }
